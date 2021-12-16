7f015240bf4f476702752c6f151211ec5c6a4100
Bryan Gurss <btgurss@gmail.com>
2021-12-15 18:35:39 -0600

    Add files via upload

--- SimpleEncryptDecryptAltSplit/AlternatingSplit.py
+++ SimpleEncryptDecryptAltSplit/AlternatingSplit.py
@@ -1,18 +1,26 @@
 def decrypt(encrypted_text, n):
     for i in range (0,n):
+        firstHalf = []
+        secondHalf = []
+        text = ""
         count = 0
-        text = []
+        count2 = 0
         for letter in encrypted_text:
-            text.append(letter)
-            if count < len(encrypted_text)/2:
-                text[2*count + 1] = letter
+            if count <= (len(encrypted_text)-1)/2:
+                firstHalf.append(letter)
             else:
-                text[2*count - 14] = letter
+                secondHalf.append(letter)
             count += 1
-        print(text)
-    print(encrypted_text)
-    return (encrypted_text)
-
+        print(f"Second Half {secondHalf}")
+        print(f"First Half {firstHalf}")
+        while count2 <= count/2:
+            try:
+                text = text + secondHalf[count2] + firstHalf[count2]
+            except:
+                text = text
+            count2 += 1
+        encrypted_text = text
+    return(text)
 
 
 def encrypt(text, n):
@@ -27,10 +35,10 @@ def encrypt(text, n):
                 odd = odd + letter
             count += 1
         text = odd + even
-        print(text)
     return (text)
 
 test = "This is a test"
-n = 15
+test2 = "012345"
+n = 3
 comes_back = encrypt(test, n)
 decrypt(comes_back, n)
